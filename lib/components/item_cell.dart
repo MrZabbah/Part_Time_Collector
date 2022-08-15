21a344507da01ed824a5ae441badee249b17f782
Sergio Mulas Azabal <56138357+MrZabbah@users.noreply.github.com>
2022-08-15 18:00:25 +0200

    ListView fixed + new trophie lock icon (#1)

--- lib/components/item_cell.dart
+++ lib/components/item_cell.dart
@@ -22,70 +22,72 @@ class ItemCell extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: InkWell(
-        onLongPress: () {
-          onUpdateCount(index);
-        },
-        onDoubleTap: () => onDelete(index),
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Ink(
-              width: MediaQuery.of(context).size.width,
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 26, 26, 26),
+      child: Material(
+        child: InkWell(
+          onLongPress: () {
+            onUpdateCount(index);
+          },
+          onDoubleTap: () => onDelete(index),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Ink(
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 26, 26, 26),
+                ),
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 6,
+                      ),
+                      Text(
+                        items[index]!.name,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
+              ClipPath(
+                clipper: HorizontalCurvyClipper(),
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 6,
+                  decoration: const BoxDecoration(
+                    //color: Color.fromARGB(255, 61, 62, 63),
+                    gradient: LinearGradient(colors: [
+                      Color.fromARGB(255, 61, 62, 63),
+                      Color.fromARGB(255, 110, 110, 110),
+                    ], stops: [
+                      0.6,
+                      0.95
+                    ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(16.0),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
                 child: Row(
                   children: [
                     SizedBox(
-                      width: MediaQuery.of(context).size.width / 6,
+                      width: MediaQuery.of(context).size.width / (6 * 6),
                     ),
-                    Text(
-                      items[index]!.name,
-                      style: const TextStyle(color: Colors.white),
+                    Icon(
+                      Icons.star,
+                      color: (items[index]!.isCompleted)
+                          ? const Color.fromARGB(255, 211, 163, 21)
+                          : const Color.fromARGB(85, 0, 0, 0),
                     ),
                   ],
                 ),
-              ),
-            ),
-            ClipPath(
-              clipper: HorizontalCurvyClipper(),
-              child: Container(
-                width: MediaQuery.of(context).size.width / 6,
-                decoration: const BoxDecoration(
-                  //color: Color.fromARGB(255, 61, 62, 63),
-                  gradient: LinearGradient(colors: [
-                    Color.fromARGB(255, 61, 62, 63),
-                    Color.fromARGB(255, 110, 110, 110),
-                  ], stops: [
-                    0.6,
-                    0.95
-                  ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.all(16.0),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width / (6 * 6),
-                  ),
-                  Icon(
-                    Icons.star,
-                    color: (items[index]!.isCompleted)
-                        ? const Color.fromARGB(255, 211, 163, 21)
-                        : const Color.fromARGB(85, 0, 0, 0),
-                  ),
-                ],
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
